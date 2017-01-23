252552b6ebb4aefe39f6b2061e4045f18befb832
Doug Baldwin <baldwin@geneseo.edu>
2017-01-23 16:03:44 -0500

    Rename 'drawRotatedCrystal' symmetry operation to 'transform4'

--- Utilities/SymmetryOps.py
+++ Utilities/SymmetryOps.py
@@ -11,11 +11,10 @@ from VectorOps import transpose, matrixMultiply
 
 
 
-# Transform (nominally by rotating, although any affine transformation can be used) a list
-# of vertices. This method receives a list of vertices in homogeneous coordinates, and
-# the transformation as a 4-by-4 matrix, and returns the list of transformed vertices. 
+# Transform a list of vertices in homogeneous coordinates by applying a 4-by-4
+# transformation matrix to them. Returns the list of transformed vertices. 
 
-def drawRotatedCrystals( vertices, transformation ) :
+def transform4( vertices, transformation ) :
 	
 	Intermediate = transpose( vertices )
 
